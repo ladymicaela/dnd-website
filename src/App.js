90d4e49d40c9e5c91118be664e48332749aae1e6
micaela <mcmoss1991@gmail.com>
2018-11-30 11:00:29 -0800

    let's see if prettier works this time

--- src/App.js
+++ src/App.js
@@ -8,35 +8,36 @@ import fullInfo from './data/dndInfo.json'
 import Ability from './components/Ability'
 
 class App extends Component {
-  state = {
-    level: 1,
-  }
- 
-  handleLevelChange = (event) => {
-    this.setState({ level: event.target.value })
-  }
+	state = {
+	level: 1,
+}
 
-  
-  render() {
-    const bonus = fullInfo.proficiencyBonus[this.state.level]
-    return (
-      <div>
-        <h1>Character Sheet</h1>
-        <select onChange={this.handleLevelChange} >
-          <option>Select Level</option>
-          {Object.keys(fullInfo.proficiencyBonus).map(level =>
-            <option key={'level-${level}'} value={level}>{level}</option>
-          )}
-        </select>
-        <h2>Proficiency Bonus</h2>
-          <div>{bonus}</div>
-        {fullInfo.abilities.map(ability =>
-            <Ability input={ability} proficiencyBonus={bonus} />
-          )
-        }
-      </div>
-    );
-  }
+handleLevelChange = (event) => {
+	this.setState({ level: event.target.value })
+}
+
+
+render() {
+const bonus = fullInfo.proficiencyBonus[this.state.level]
+return (
+<Ability one={oneLongName} two={twolongname} three={threelongname} four={fourlongname} />
+<div>
+<h1>Character Sheet</h1>
+<select onChange={this.handleLevelChange} >
+<option>Select Level</option>
+{Object.keys(fullInfo.proficiencyBonus).map(level =>
+<option key={'level-${level}'} value={level}>{level}</option>
+)}
+</select>
+<h2>Proficiency Bonus</h2>
+<div>{bonus}</div>
+{fullInfo.abilities.map(ability =>
+<Ability input={ability} proficiencyBonus={bonus} />
+)
+}
+</div>
+);
+}
 }
 
 export default App;
